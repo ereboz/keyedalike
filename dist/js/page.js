f05eb57119095e3a5ce2a85e71430ac0c342a776
ereboz <ereboz8@gmail.com>
2017-05-08 09:36:30 -0700

    Moved topnavbar affix code in page.js into document ready func

--- dist/js/page.js
+++ dist/js/page.js
@@ -2,18 +2,18 @@
  * Created by javaBoxer on 3/23/2017.
  */
 
-// affixes top nav to be sticky
-$('#topnavbar').affix({
-  offset: {
-    top: $('#banner').height()
-  }
-});
-
 // Assuming backend is writing the sale price strike through, we can toggle sale sticker in upper left of product.
 // iterate over each product-wrapper and search for value of retail-price.
 // if not empty, then it's on sale. Toggle Less class to display sale sticker in upper left of parent wrapper
 
 $(document).ready(function(){
+    
+    // affixes top nav to be sticky
+    $('#topnavbar').affix({
+        offset: {
+            top: $('#banner').height()
+        }
+    });
 
     $('.product-wrapper').each(function () {
         var retailValue = $(this).find('.retail-price');
