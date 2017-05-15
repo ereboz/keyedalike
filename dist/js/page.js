ad56585ccc1d55ba44a62d14094b71e01d59f47e
ereboz <ereboz8@gmail.com>
2017-05-15 09:54:23 -0700

    KA-62 Contact Us page Scrolling – Affix issue affecting any page with height < offset - Bootstrap Bug

--- dist/js/page.js
+++ dist/js/page.js
@@ -2,19 +2,30 @@
  * Created by javaBoxer on 3/23/2017.
  */
 
-// Assuming backend is writing the sale price strike through, we can toggle sale sticker in upper left of product.
-// iterate over each product-wrapper and search for value of retail-price.
-// if not empty, then it's on sale. Toggle Less class to display sale sticker in upper left of parent wrapper
 
 $(document).ready(function(){
     
-    // affixes top nav to be sticky
-    $('#topnavbar').affix({
+    //// affixes top nav to be sticky
+    
+    $(".wrapper-subnav").affix({
         offset: {
             top: $('#banner').height()
         }
     });
+    
+    $("#topnavbar").height($(".wrapper-subnav").height());
+    
+    // $('#topnavbar').affix({
+    //     offset: {
+    //         top: $('#banner').height()
+    //     }
+    // });
 
+// Add Sale sticker to sale items
+// Assuming backend is writing the sale price strike through, we can toggle sale sticker in upper left of product.
+// iterate over each product-wrapper and search for value of retail-price.
+// if not empty, then it's on sale. Toggle Less class to display sale sticker in upper left of parent wrapper
+    
     $('.product-wrapper').each(function () {
         var salePrice = $(this).find('.sale-price');        // target to center price vertically if no retail price
         var retailValue = $(this).find('.retail-price');
